76283090bf7d8aa3d0f890f3b534d5f277beee1d
Rocío orellana Díaz <rocio.orellana.ing@gmail.com>
2026-01-16 00:40:02 -0300

    Configuracion de PyMySQL para despliegue en Vercel

--- colegio/colegio/settings.py
+++ colegio/colegio/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
+import pymysql
 
+pymysql.version_info = (1, 4, 6, 'final', 0) 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
